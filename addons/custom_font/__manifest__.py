81010737b4f050711f93250e40dccb2adbd9d591
Rossionn <nro@umamimanagement.be>
2026-01-29 10:22:22 +0100

    Fix manifests to valid Odoo Python dict format

--- addons/custom_font/__manifest__.py
+++ addons/custom_font/__manifest__.py
@@ -1,9 +1,9 @@
 ﻿{
-  "name": "custom_font",
-  "version": "19.0.1.0.0",
-  "summary": "Stub module to satisfy DB after migration/import",
-  "depends": ["base"],
-  "installable": true,
-  "application": false,
-  "license": "LGPL-3"
+    "name": "custom_font",
+    "version": "19.0.1.0.0",
+    "summary": "Stub module to satisfy DB after migration/import",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
 }
