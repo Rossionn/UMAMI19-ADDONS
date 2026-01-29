4d0b33a8481dc786f8486df0daac3aaf1feb5cb6
Rossionn <nro@umamimanagement.be>
2026-01-29 09:52:31 +0100

    Initial placeholder addons

---
+++ addons/custom_font/__manifest__.py
@@ -0,0 +1,9 @@
+﻿{
+  "name": "custom_font",
+  "version": "19.0.1.0.0",
+  "summary": "Stub module to satisfy DB after migration/import",
+  "depends": ["base"],
+  "installable": true,
+  "application": false,
+  "license": "LGPL-3"
+}
---
+++ addons/website_pdf_preview_snippet/__manifest__.py
@@ -0,0 +1,9 @@
+﻿{
+  "name": "website_pdf_preview_snippet",
+  "version": "19.0.1.0.0",
+  "summary": "Stub module to satisfy DB after migration/import",
+  "depends": ["base","website"],
+  "installable": true,
+  "application": false,
+  "license": "LGPL-3"
+}
